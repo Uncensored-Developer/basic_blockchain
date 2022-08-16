b58153537f5325df2c4dcc239833dd5d0818a498
Osezele Orukpe <osezele.o@iflux.app>
2022-08-16 03:12:22 +0100

    Add methods to persist txns, add txns, apply txns to the state.

---
+++ database/state.go
@@ -0,0 +1,107 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type State struct {
+	Balances  map[Account]uint
+	txMempool []Tx
+	dbFile    *os.File
+}
+
+func NewStateFromDisk() (*State, error) {
+	//get current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	genesisFilePath := filepath.Join(cwd, "database", "genesis.json")
+	gen, err := loadGenesis(genesisFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	balances := make(map[Account]uint)
+	for account, balance := range gen.Balances {
+		balances[account] = balance
+	}
+
+	txDbFilePath := filepath.Join(cwd, "database", "tx.db")
+	f, err := os.OpenFile(txDbFilePath, os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+	state := &State{balances, make([]Tx, 0), f}
+
+	//iterate over each txn in tx.db file
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var tx Tx
+		err := json.Unmarshal(scanner.Bytes(), &tx)
+		if err != nil {
+			return nil, err
+		}
+
+		//Rebuild the state (user balances) as a series of events
+		if err := state.apply(tx); err != nil {
+			return nil, err
+		}
+	}
+	return state, nil
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if tx.Value > s.Balances[tx.From] {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+	return nil
+}
+
+func (s *State) Add(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+
+	s.txMempool = append(s.txMempool, tx)
+	return nil
+}
+
+func (s *State) Persist() error {
+	//Make a copy of the mempool because s.txMempool will be modified
+	mempool := make([]Tx, len(s.txMempool))
+	copy(mempool, s.txMempool)
+
+	for i := 0; i < len(mempool); i++ {
+		txJson, err := json.Marshal(mempool[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err = s.dbFile.Write(append(txJson, '\n')); err != nil {
+			return err
+		}
+
+		//remove persisted TX from the mempool
+		s.txMempool = s.txMempool[1:]
+	}
+	return nil
+}
